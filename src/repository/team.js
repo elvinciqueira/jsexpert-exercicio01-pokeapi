971f209e13a795d840662fd288d2d35c1d88bc0f
Elvin Ciqueira <elvin.ciqueira@ferreri.co>
2022-05-18 17:51:59 -0300

    refactor: functional approach

--- src/repository/team.js
+++ src/repository/team.js
@@ -2,7 +2,7 @@ const { default: axios } = require('axios');
 
 const API_URL = 'https://pokeapi.co/api/v2/pokemon';
 
-async function getPokemons() {
+async function listPokemons() {
   const response = await axios.get(API_URL);
   return response.data.results;
 }
@@ -13,6 +13,6 @@ async function getPokemon(url) {
 }
 
 module.exports = {
-  getPokemons,
+  listPokemons,
   getPokemon,
 };
--- src/service/team.js
+++ src/service/team.js
@@ -1,34 +1,30 @@
-const { getPokemon, getPokemons } = require('../repository/team');
+const { getPokemon, listPokemons } = require('../repository/team');
 
 async function getTeam(teamSize = 3) {
-  const pokemons = await getRandomPokemons(teamSize);
-  return pokemons;
+  return listPokemons()
+    .then(getRandomPokemons(teamSize))
+    .then(extractPokemonData);
 }
 
-async function getRandomPokemons(quantity) {
-  const pokemons = await getPokemons();
-  const pokemonRaw = getMultipleRandomItemsFromArray(pokemons, quantity);
-
-  return Promise.all(extractPokemonData(pokemonRaw));
-}
-
-function getMultipleRandomItemsFromArray(list, quantity) {
-  return [...Array(quantity)].map(() => getRandomItemFromArray(list));
-}
+const getRandomPokemons = (quantity) => (pokemons) => {
+  return [...Array(quantity)].map(() => getRandomItemFromArray(pokemons));
+};
 
 function getRandomItemFromArray(list) {
   return list[Math.floor(Math.random() * list.length)];
 }
 
 function extractPokemonData(pokemonRaw) {
-  return pokemonRaw.map(async (pokemon) => {
-    const pokemonData = await getPokemon(pokemon.url);
+  return Promise.all(
+    pokemonRaw.map(async (pokemon) => {
+      const pokemonData = await getPokemon(pokemon.url);
 
-    return {
-      name: pokemonData.name,
-      moves: pokemonData.moves.slice(0, 3).map((move) => move.move.name),
-    };
-  });
+      return {
+        name: pokemonData.name,
+        moves: pokemonData.moves.slice(0, 3).map((move) => move.move.name),
+      };
+    })
+  );
 }
 
 module.exports = {
